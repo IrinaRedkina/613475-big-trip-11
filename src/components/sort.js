ba9cdc19f12dc65d7fd1a2c349b9b455970a5820
Ирина <taffyself@gmail.com>
2020-04-27 17:38:09 +0300

    Личный проект: разделяй и властвуй (Часть 2)
    Реализована сортировка точек маршрута

--- src/components/sort.js
+++ src/components/sort.js
@@ -1,25 +1,33 @@
 import AbstractComponent from './abstract-component';
 
+export const SortType = {
+  PRICE: `price`,
+  TIME: `time`,
+  DEFAULT: `default`,
+};
+
+const SORT_BY_DAY_TEXT = `Day`;
+
 const createSortTemplate = () => {
   return (
     `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
-      <span class="trip-sort__item  trip-sort__item--day">Day</span>
+      <span class="trip-sort__item  trip-sort__item--day">${SORT_BY_DAY_TEXT}</span>
 
       <div class="trip-sort__item  trip-sort__item--event">
         <input id="sort-event" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-event" checked>
-        <label class="trip-sort__btn" for="sort-event">Event</label>
+        <label class="trip-sort__btn" for="sort-event" data-sort-type="${SortType.DEFAULT}">Event</label>
       </div>
 
       <div class="trip-sort__item  trip-sort__item--time">
         <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time">
-        <label class="trip-sort__btn  trip-sort__btn--active  trip-sort__btn--by-increase" for="sort-time">
+        <label class="trip-sort__btn  trip-sort__btn--active  trip-sort__btn--by-increase" for="sort-time" data-sort-type="${SortType.TIME}">
           Time
         </label>
       </div>
 
       <div class="trip-sort__item  trip-sort__item--price">
         <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price">
-        <label class="trip-sort__btn" for="sort-price">
+        <label class="trip-sort__btn" for="sort-price" data-sort-type="${SortType.PRICE}">
           Price
         </label>
       </div>
@@ -30,7 +38,34 @@ const createSortTemplate = () => {
 };
 
 export default class Sort extends AbstractComponent {
+  constructor() {
+    super();
+
+    this._currenSortType = SortType.DEFAULT;
+  }
+
   getTemplate() {
     return createSortTemplate();
+  }
+
+  setSortTypeChangeHandler(handler) {
+    this.getElement().addEventListener(`click`, (evt) => {
+      if (!evt.target.dataset.sortType) {
+        return;
+      }
+
+      const sortType = evt.target.dataset.sortType;
+
+      if (this._currenSortType === sortType) {
+        return;
+      }
+
+      this._currenSortType = sortType;
+
+      const itemSortDay = this.getElement().querySelector(`.trip-sort__item--day`);
+      itemSortDay.innerHTML = this._currenSortType === SortType.DEFAULT ? SORT_BY_DAY_TEXT : ``;
+
+      handler(this._currenSortType);
+    });
   }
 }
